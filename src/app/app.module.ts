013571e13a613961315179a6c78e6f798b58b712
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-08-24 14:59:33 +0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
+const FirebaseModules = [
+  AngularFireModule.initializeApp(environment.firebase),
+  AngularFireAuthModule,
+  AngularFirestoreModule,
+  AngularFireStorageModule,
+  AngularFireDatabaseModule
+];
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ...FirebaseModules
   ],
   providers: [],
   bootstrap: [AppComponent]
