d935e68cf84ee49bf5e931e930d416a3cfa67946
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-08-26 15:18:14 +0300

    update

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../../shared/services/auth.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
+  @Input() set formData(data: any) {
+    let email = data.email || '';
+    let remember = data.rememeber || false;
+    if(localStorage.getItem('saved_email') && email === '') {
+      email = localStorage.getItem('saved_email');
+      remember = true;
+    }
+    this.loginForm.setValue({
+      email: email,
+      password: data.password,
+      remember: remember
+    })
+  }
+  @Output() formDataEmitter = new EventEmitter<any>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public loginForm: UntypedFormGroup;
+  constructor(
+    public fb: UntypedFormBuilder,
+    private router: Router,
+    public authService: AuthService
+    ) {
+      this.loginForm = this.fb.group({
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required]],
+        remember: [false, []],
+      })
   }
 
+  sendFormData(form: UntypedFormGroup) {
+    this.authService.signUp(form.value.email, form.value.password);
+  }
+
+  loginByGoogle(event: any) {
+    console.log('Login By Google');
+    this.authService.googleAuth();
+  }
 }
