4afc0fa9d8a7d6240ab6c6545c9720b68d238479
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-08-29 16:37:21 +0300

    Add data visualization to dashboard

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -7,12 +7,14 @@ import { AuthService } from '../../shared/services/auth.service';
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
-
+  public userData: any;
   constructor(
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.userData = this.authService.userData
+    console.log(this.userData);
   }
 
   logout() {
